7f42dad33db2bfbd8fe4aea799e9f0f49f4476b8
bells <the-bells-are-very-loud-today@proton.me>
2026-02-21 10:27:07 -0700

    some random changes ((straight to master cause they are so misc)) mostly due to sys update

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -5,4 +5,5 @@ alias gm="git merge --no-ff"
 alias gblt="git branch --list"
 alias gc="git commit --verbose --message"
 alias vi=nvim
-alias vid=i"nvim -d"
+alias vid="nvim -d"
+alias sp="_ chmod 660 /dev/tty && ncspot"
