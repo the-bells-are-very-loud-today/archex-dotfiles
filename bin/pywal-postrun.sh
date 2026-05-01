82e2961007eb4da5d009fe3e0b97053528f18e8c
bells <the-bells-are-very-loud-today@proton.me>
2026-04-30 22:37:37 -0600

    integreated dunst into pywal (PARTIALLY)

--- bin/pywal-postrun.sh
+++ bin/pywal-postrun.sh
@@ -9,7 +9,9 @@ pkill -SIGUSR2 waybar
 # The name for the cava color file can be anything really, I just used this to go along with my other templates.
 # Do note, that if you want to change any configuration in cava, you WILL have to do it through the configuration file in the TEMPLATES FOLDER.
 cp ~/.cache/wal/colors-cava.conf ~/.config/cava/config
+cp ~/.cache/wal/colors-dunst ~/.config/dunst/dunstrc
 
 pkill -USR2 cava # Reloads cava's colorscheme configuration
 #reloads eww midgets to match theme
 eww reload
+dunstctl reload
